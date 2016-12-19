84d259ea8ae26f3f9117767925eef970f7d32ad1
phamtlsoft <phamtlsoft@gmail.com>
2016-12-19 17:42:35 +0700

    Initial commit

---
+++ src/main/java/springmvc/service/HelloWorldService.java
@@ -0,0 +1,13 @@
+package springmvc.service;
+
+/**
+ * 
+ * @author PHAMTLSOFT
+ *
+ */
+public interface HelloWorldService {
+	
+	public static final String SERVICE_NAME = "HELLO WORLD SERVICE";
+	public void sayHelloWorld();
+
+}
---
+++ src/main/java/springmvc/service/impl/HelloWorldServiceImpl.java
@@ -0,0 +1,15 @@
+package springmvc.service.impl;
+
+
+import springmvc.service.HelloWorldService;
+
+//@Service("helloWorldService")
+public class HelloWorldServiceImpl implements HelloWorldService {
+
+	@Override
+	public void sayHelloWorld() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
